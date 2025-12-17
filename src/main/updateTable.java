ccb74b21036a6846159c8470c3cd49ee42ade9ab
Enrique-M1 <emartinez.sebo@gmail.com>
2025-12-17 15:31:46 -0600

    Added functionality to add a record to a table

--- src/main/updateTable.java
+++ src/main/updateTable.java
@@ -2,14 +2,16 @@ package main;
 
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
 import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class updateTable {
-    public static void updateTable(DynamoDbClient client, String tableName, int numberOfAttributes){
+    public static void updateTable(DynamoDbClient client, String tableName){
         /* TODO:
             1.) Delete a row in the table
             2.) Delete a column in the table
@@ -40,26 +42,50 @@ public class updateTable {
 
                 // Add a record to the table
                 case "3":
-                    /* TODO:
-                        Figure out a way to get the attribute list from the previous record
-                        in a specific table.
-                    */
-                    String keyName = "";
-                    HashMap<String, AttributeValue> keyToGet = new HashMap<>();
-                    Map<String, AttributeValue> row = new HashMap<>();
-                    keyToGet.put(keyName, AttributeValue.builder().s().build());
-                    Set<String> attributeNamesSet = item.keySet();
-                    List<String> attributeNamesList = new ArrayList<>(attributeNamesSet);
+                    System.out.println("What table would you like to add a record to?");
+                    String updateTable = input.nextLine();
+                    String flag = "y";
 
-                    GetItemRequest request = GetItemRequest.builder()
-                            .tableName(tableName)
-                            .key(keyToGet)
-                            .build();
+                    while (true) { // While we are adding records, continue
+
+                        if (flag.equalsIgnoreCase("y")|| flag.equalsIgnoreCase("yes")) {
+                            // Update the table
+                            while (flag.equals("y") || flag.equals("yes")) { // Create each row
+                                System.out.println("Enter the amount of attributes for this row: ");
+                                int numberOfAttributes = Integer.parseInt(input.nextLine());
+                                Map<String, AttributeValue> row = new HashMap<>();
+                                String attributeName;
 
 
-                    for (int count = 0; count < numberOfAttributes; count++) { // Iterate through each attribute
-                        System.out.print("Enter attribute " + attributeName.get(count) + " value: ");
-                        row.put(attributeName.get(count), AttributeValue.builder().s(input.nextLine()).build());
+                                for (int count = 1; count < numberOfAttributes + 1; count++) { // Iterate through each column
+                                    System.out.print("Enter attribute " + count + " value: ");
+                                    System.out.println("Enter attribute name: ");
+                                    attributeName = input.nextLine();
+
+                                    row.put(attributeName, AttributeValue.builder().s(input.nextLine()).build());
+                                }
+
+                                // Put item in the table
+                                PutItemRequest putRequest = PutItemRequest.builder()
+                                        .tableName(tableName).item(row).build();
+
+                                try { // Put the item in the table
+                                    client.putItem(putRequest);
+                                } catch (DynamoDbException e) {
+                                    System.err.println(e.getMessage());
+                                    return;
+                                }
+                                System.out.print("Add another? (y or yes): ");
+                                flag = input.nextLine().toLowerCase();
+                            }
+
+
+                            System.out.println("Add another record? (y or yes)");
+                            flag = input.nextLine();
+
+                        } else { // Done adding records, break
+                            break;
+                        }
                     }
 
                     return;
